ca43539e8298e894f7ba2e2ebd854266586abde6
Jacob Langley <jacob.langley@ghtns.com>
2019-07-03 15:23:08 -0700

    Add an initial draft of an easylist rule to little snitch tool

---
+++ bin/easylist2littlesnitch.py
@@ -0,0 +1,37 @@
+#!env python3
+
+# filters explained here:
+# https://adblockplus.org/en/filter-cheatsheet#blocking2
+
+
+
+input_file = open("easylist.txt")
+
+output_data = {}
+output_data["description"] = "Blocking domains based on easylist"
+output_data["name"] = "EasyList"
+output_data["rules"] = [];
+
+for line in input_file:
+	line = line.strip()
+
+	# starting with || is a domain rule
+	if line.startswith("||"):
+		line = line[2:]
+		separator = line.find("^")
+
+		# if we find a separator, then this is a rule where 
+		# we block if the domain contains this.  i.e  example.com blocks this.example.com
+		if separator != -1:
+			deny_rule = {}
+			deny_rule["action"] = "deny"
+			deny_rule["creationDate"] = 0.0
+			deny_rule["modificationDate"] = 0.0
+			deny_rule["owner"] = "me"
+			deny_rule["process"] = "any"
+			deny_rule["remote-domains"] = line[:separator]
+			output_data["rules"].append(deny_rule)
+
+input_file.close()
+
+print(output_data)
