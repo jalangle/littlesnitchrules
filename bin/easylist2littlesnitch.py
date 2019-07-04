1d1dc6fbac19d8302caa3b6949275c894cfd7541
Jacob Langley <jacob.langley@ghtns.com>
2019-07-04 12:13:18 -0700

    easylist2littlesnitch.py - minor refactoring

--- bin/easylist2littlesnitch.py
+++ bin/easylist2littlesnitch.py
@@ -5,51 +5,56 @@ import io
 import json
 import requests
 
-def get_easylist_file():
-	url = "https://easylist.to/easylist/easylist.txt"
-	response = requests.get(url)
-	return io.StringIO(response.text)
+def get_easylist_file(input_path):
+	if input_path == None:
+		url = "https://easylist.to/easylist/easylist.txt"
+		response = requests.get(url)
+		return io.StringIO(response.text)
+	else:
+		return open(args.input_path, 'r')
 
-parser = argparse.ArgumentParser(description='Turn domains from an AdBlock formatted filter list into a littlesnitch lsrules file')
-parser.add_argument('--input', dest='input_path', action='store', default=None, help='Path to an AdBlock filter formatted file')
-parser.add_argument('--output', dest='output_path', action='store', default=None, help='Path to output file')
-args = parser.parse_args()
+def dump_data(output_path, output_data):
+	if(output_path == None):
+		print(json.dumps(output_data, indent=2))
+	else:
+		output_file = open(output_path, 'w')
+		json.dump(output_data, output_file, indent=2)
+		output_file.close()
 
-# filters explained here:
-# https://adblockplus.org/en/filter-cheatsheet#blocking2
-if args.input_path == None:
-	input_file = get_easylist_file()
-else:
-	input_file = open(args.input_path, 'r')
+def main():
+	parser = argparse.ArgumentParser(description='Turn domains from an AdBlock formatted filter list into a littlesnitch lsrules file')
+	parser.add_argument('--input', dest='input_path', action='store', default=None, help='Path to an AdBlock filter formatted file')
+	parser.add_argument('--output', dest='output_path', action='store', default=None, help='Path to output file')
+	args = parser.parse_args()
 
-output_data = {}
-output_data["description"] = "Blocking domains based on easylist"
-output_data["name"] = "EasyList"
-output_data["rules"] = [];
+	input_file = get_easylist_file(args.input_path)
 
-for line in input_file:
-	line = line.strip()
+	output_data = {}
+	output_data["description"] = "Blocking domains based on easylist"
+	output_data["name"] = "EasyList"
+	output_data["rules"] = [];
 
-	# starting with || is a domain rule
-	if line.startswith("||"):
-		line = line[2:]
-		separator = line.find("^")
+	# filters explained here:
+	# https://adblockplus.org/en/filter-cheatsheet#blocking2
+	with get_easylist_file(args.input_path) as input_file:
+		for line in input_file:
+			line = line.strip()
 
-		# if we find a separator, then this is a rule where 
-		# we block if the domain contains this.  i.e  example.com blocks this.example.com
-		if separator != -1:
-			deny_rule = {}
-			deny_rule["action"] = "deny"
-			deny_rule["owner"] = "me"
-			deny_rule["process"] = "any"
-			deny_rule["remote-domains"] = line[:separator]
-			output_data["rules"].append(deny_rule)
+			# starting with || is a domain rule
+			if line.startswith("||"):
+				line = line[2:]
+				separator = line.find("^")
 
-input_file.close()
+				# if we find a separator, then this is a rule where
+				# we block if the domain contains this.  i.e  example.com blocks this.example.com
+				if separator != -1:
+					deny_rule = {}
+					deny_rule["action"] = "deny"
+					deny_rule["owner"] = "me"
+					deny_rule["process"] = "any"
+					deny_rule["remote-domains"] = line[:separator]
+					output_data["rules"].append(deny_rule)
 
-if(args.output_path == None):
-	print(json.dumps(output_data, indent=2))
-else:
-	output_file = open(args.output_path, 'w')
-	json.dump(output_data, output_file, indent=2)
-	output_file.close()
+	dump_data(args.output_path, output_data)
+
+main()
