5da7c1efcc49babcc9902470bdd3fd2d9c7d36ad
Jacob Langley <jacob.langley@ghtns.com>
2019-07-04 18:39:05 -0700

    geturl.py: allow overriding the branch

--- bin/geturl.py
+++ bin/geturl.py
@@ -13,6 +13,8 @@ def get_repo_from_url(url):
 def collect_all_rule_files(working_dir):
 	rule_file_urls = []
 
+	# this should really enumerate the repo files for lsrules rather than
+	# the file system in case branches are different...
 	for root, dirs, files in os.walk(working_dir, topdown=True):
 		for name in files:
 			if name.endswith('.lsrules'):
@@ -23,11 +25,19 @@ def collect_all_rule_files(working_dir):
 	return rule_file_urls
 
 def main():
-	repo = Repo(".")
-	origin = repo.remote()
-	repo_name = get_repo_from_url(origin.url)
-	repo_branch = repo.active_branch
+	parser = argparse.ArgumentParser(description='Get github raw URL for a specific file')
+	parser.add_argument('--branch', dest='branch', action='store', default=None, help='Branch to use.  Current branch if unspecified.')
+	args = parser.parse_args()
 
+	repo = Repo(".")
+	repo_branch = repo.active_branch
+	if args.branch != None:
+		if args.branch not in repo.branches:
+			print("Not a valid branch: " + args.branch)
+			return
+		repo_branch = args.branch
+
+	repo_name = get_repo_from_url(repo.remote().url)
 	github_raw_url_base = "https://raw.githubusercontent.com/" + str(repo_name) + "/" + str(repo_branch)
 
 	rule_files = collect_all_rule_files(repo.working_tree_dir)
